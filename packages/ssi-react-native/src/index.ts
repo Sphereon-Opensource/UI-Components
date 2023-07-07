3dc2ef0b40b2d6c454f07372221bdfa926928c6b
A.G.J. Cate <brummos@gmail.com>
2023-07-07 14:55:50 +0200

    WAL-600 updated exports

--- packages/ssi-react-native/src/index.ts
+++ packages/ssi-react-native/src/index.ts
@@ -1,4 +1,15 @@
 import SSIStatusLabel from './components/labels/SSIStatusLabel'
 import SSICardView from './components/views/SSICardView'
+import SSICheckmarkBadge from './components/assets/badges/SSICheckmarkBadge'
+import SSIExclamationMarkBadge from './components/assets/badges/SSIExclamationMarkBadge'
+import SSIPlaceholderLogo from './components/assets/logos/SSIPlaceholderLogo'
+import SSILogo from './components/assets/logos/SSILogo'
 
-export {SSICardView, SSIStatusLabel}
+export {
+    SSICardView,
+    SSIStatusLabel,
+    SSICheckmarkBadge,
+    SSIExclamationMarkBadge,
+    SSIPlaceholderLogo,
+    SSILogo
+}
