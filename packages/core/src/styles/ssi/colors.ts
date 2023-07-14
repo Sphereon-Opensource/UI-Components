cb9548a9f2e62b5ca21e9b6411fbd16b8e861571
A.G.J. Cate <brummos@gmail.com>
2023-07-14 14:26:22 +0200

    WAL-600 removed unnecessary colors

--- packages/core/src/styles/ssi/colors.ts
+++ packages/core/src/styles/ssi/colors.ts
@@ -9,66 +9,6 @@ export const backgroundColors: Record<BackgroundColor, string> = {
   secondaryLight: '#E3E3FF',
 }
 
-type AlertColor = 'primaryLight' | 'secondaryLight'
-
-export const alertColors: Record<AlertColor, string> = {
-  primaryLight: '#FBFBFB',
-  secondaryLight: '#EBEBEB',
-}
-
-type HighlightColor = 'dark'
-
-export const highlightColors: Record<HighlightColor, string> = {
-  dark: '#3B425E',
-}
-
-type BorderColor = 'dark' | 'light'
-
-export const borderColors: Record<BorderColor, string> = {
-  dark: '#404D7A',
-  light: '#E3E3E3',
-}
-
-type ProfileColor = 100 | 200 | 300 | 400 | 500
-
-export const profileColors: Record<ProfileColor, string> = {
-  100: '#EE5209',
-  200: '#FF9900',
-  300: '#5BDED3',
-  400: '#0B81FF',
-  500: '#BD2DFF',
-}
-
-type GradientProperties = {
-  primaryColor: string
-  secondaryColor: string
-}
-
-type GradientColor = 100 | 200 | 300 | 400 | 500
-
-export const gradientColors: Record<GradientColor, GradientProperties> = {
-  100: {
-    primaryColor: 'rgba(111,105,224,1)',
-    secondaryColor: 'rgba(69,64,164,1)',
-  },
-  200: {
-    primaryColor: 'rgba(250,182,81,1)',
-    secondaryColor: 'rgba(202,122,2,1)',
-  },
-  300: {
-    primaryColor: 'rgba(108,240,228,1)',
-    secondaryColor: 'rgba(0,169,153,1)',
-  },
-  400: {
-    primaryColor: 'rgba(50,156,245,1)',
-    secondaryColor: 'rgba(0,91,168,1)',
-  },
-  500: {
-    primaryColor: 'rgba(188,45,255,1)',
-    secondaryColor: 'rgba(122,3,177,1)',
-  },
-}
-
 // TODO WAL-303 add support for randomized colors to avoid getting the same color multiple times
 type CredentialCardColor = 'default'
 
