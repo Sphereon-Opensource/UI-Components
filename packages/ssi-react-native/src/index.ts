be01f78d1233a4d665691a82de3686d9c8fd396c
A.G.J. Cate <brummos@gmail.com>
2023-10-25 11:31:01 +0200

    VDX-255 rn export revert

--- packages/ssi-react-native/src/index.ts
+++ packages/ssi-react-native/src/index.ts
@@ -1,2 +1,18 @@
-export * from './components'
+import SSIStatusLabel from './components/labels/SSIStatusLabel'
+import SSICredentialCardView from './components/views/SSICredentialCardView'
+import SSICredentialMiniCardView from './components/views/SSICredentialMiniCardView'
+import SSICheckmarkBadge from './components/assets/badges/SSICheckmarkBadge'
+import SSIExclamationMarkBadge from './components/assets/badges/SSIExclamationMarkBadge'
+import SSIPlaceholderLogo from './components/assets/logos/SSIPlaceholderLogo'
+import SSILogo from './components/assets/logos/SSILogo'
 export * from './styles/components/fonts'
+
+export {
+    SSICredentialCardView,
+    SSICredentialMiniCardView,
+    SSIStatusLabel,
+    SSICheckmarkBadge,
+    SSIExclamationMarkBadge,
+    SSIPlaceholderLogo,
+    SSILogo
+}
