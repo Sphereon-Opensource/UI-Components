c102fbbd94334b64e098677378cd056478a35db8
Maikel Maas <mmaas@sphereon.com>
2023-11-02 11:18:41 +0100

    VDX-258 Used deconstruction and set default value of size

--- packages/ssi-react-native/src/components/indicators/SSIActivityIndicator/index.tsx
+++ packages/ssi-react-native/src/components/indicators/SSIActivityIndicator/index.tsx
@@ -1,11 +1,12 @@
 import React, {FC} from 'react'
 import {ActivityIndicator, ColorValue, StyleProp, ViewStyle} from 'react-native'
 
-type Props = {size: number | 'small' | 'large'; color?: ColorValue; style?: StyleProp<ViewStyle>}
+type Props = {size?: number | 'small' | 'large'; color?: ColorValue; style?: StyleProp<ViewStyle>}
 
-const SSIActivityIndicator: FC<Props> = (props: Props): React.ReactElement => {
+const SSIActivityIndicator: FC<Props> = ({color, style, size = 80}): React.ReactElement => {
+
   return (
-     <ActivityIndicator style={props.style} size={props.size} color={props.color} />
+     <ActivityIndicator style={style} size={size} color={color} />
   )
 }
 
