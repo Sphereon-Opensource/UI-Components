1958cf611a62d558d4d8f821780fb2e3fa0945d4
Maikel Maas <mmaas@sphereon.com>
2023-11-02 15:29:19 +0100

    VDX-61 Fixed code style

--- packages/ssi-react-native/src/components/indicators/SSIActivityIndicator/index.tsx
+++ packages/ssi-react-native/src/components/indicators/SSIActivityIndicator/index.tsx
@@ -2,9 +2,14 @@ import React, {FC} from 'react'
 import {ActivityIndicator, ColorValue, StyleProp, ViewStyle} from 'react-native'
 import {elements} from '@sphereon/ui-components.core'
 
-type Props = {size?: number | 'small' | 'large'; color?: ColorValue; style?: StyleProp<ViewStyle>}
+type Props = {
+  size?: number | 'small' | 'large'
+  color?: ColorValue
+  style?: StyleProp<ViewStyle>
+}
 
-const SSIActivityIndicator: FC<Props> = ({color = elements.blue, style, size = 80}): React.ReactElement => {
+const SSIActivityIndicator: FC<Props> = (props: Props): React.ReactElement => {
+  const {color = elements.blue, style, size = 80} = props
   return <ActivityIndicator style={style} size={size} color={color} />
 }
 
