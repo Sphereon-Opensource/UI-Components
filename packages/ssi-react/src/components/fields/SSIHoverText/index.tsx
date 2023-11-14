23ba373c7a77b3dd60f01b74e81a3b0a9e87fa54
sksadjad <skhoshkhou@sphereon.com>
2023-11-14 11:37:15 +0100

    chore: aded default value (false) for enableHover option

--- packages/ssi-react/src/components/fields/SSIHoverText/index.tsx
+++ packages/ssi-react/src/components/fields/SSIHoverText/index.tsx
@@ -12,12 +12,13 @@ export type Props = {
 }
 
 const SSIHoverText: FC<Props> = (props: Props): ReactElement => {
-  const {text, opts } = props
-  const value = opts?.truncationLength ? text.substring(0, opts.truncationLength) : text
+  const {text, opts= {}} = props
+  const {truncationLength, enableHover = false} = opts
+  const value = truncationLength ? text.substring(0, truncationLength) : text
   return (
     <Container>
       <Text>{value}</Text>
-      {opts?.enableHover && <TextHover>{text}</TextHover>}
+      {enableHover && <TextHover>{text}</TextHover>}
     </Container>
   )
 }
