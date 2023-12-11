cb45970a463cd17da535e89b7097f50e2ee800cd
Maikel Maas <mmaas@sphereon.com>
2023-12-11 14:13:20 +0100

    DPP-93 - Fixed new trailing line, constant case and folder naming

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -3,5 +3,5 @@ import Localization from './localization/Localization'
 export * from './utils'
 export * from './types'
 export * from './styles'
-export * from './regex'
+export * from './regexes'
 export {Localization}
