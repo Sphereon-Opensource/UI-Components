c8885e82d8b36ec6f2564dcc33d4aa4933a759ea
sksadjad <skhoshkhou@sphereon.com>
2024-02-01 16:32:56 +0100

    chore: prettier

--- packages/core/src/regexes/index.ts
+++ packages/core/src/regexes/index.ts
@@ -1,1 +1,1 @@
-export const emailAddressValidationRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+export const emailAddressValidationRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
