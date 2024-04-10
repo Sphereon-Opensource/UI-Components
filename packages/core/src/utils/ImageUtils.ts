09743f357092048bf50622e69ab3845e44946a4c
A.G.J. Cate <brummos@gmail.com>
2024-04-10 11:21:17 +0200

    chore: updated base64Uri validation regex

--- packages/core/src/utils/ImageUtils.ts
+++ packages/core/src/utils/ImageUtils.ts
@@ -1,10 +1,12 @@
+import {base64UriValidationRegex} from '../regexes';
+
 export const calculateAspectRatio = (width: number, height: number): number => {
   return width / height
 }
 
 export const isBase64ImageUri = (value: unknown): boolean => {
   if (typeof value === 'string') {
-    return /^data:image\//.test(value)
+    return base64UriValidationRegex.test(value)
   }
 
   return false
