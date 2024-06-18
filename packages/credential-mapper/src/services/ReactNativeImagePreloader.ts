2db69c759dd8190ec8dcbfe26b271ab69b90e668
sksadjad <skhoshkhou@sphereon.com>
2024-06-18 12:25:41 +0200

    chore: refactored to use fastImage again

--- packages/credential-mapper/src/services/ReactNativeImagePreloader.ts
+++ packages/credential-mapper/src/services/ReactNativeImagePreloader.ts
@@ -1,16 +1,8 @@
 import {IPreloadImage, IImagePreloader} from './IPreloadImage'
+const FastImage = require('react-native-fast-image')
 
 export class ReactNativeImagePreloader implements IImagePreloader {
   async preload(sources: Array<IPreloadImage>): Promise<void> {
-    const promises = sources.map(
-      source =>
-        new Promise<void>((resolve, reject) => {
-          const img = new Image()
-          img.src = source.uri
-          img.onload = () => resolve()
-          img.onerror = error => reject(error)
-        }),
-    )
-    await Promise.all(promises)
+    FastImage.preload(sources)
   }
 }
